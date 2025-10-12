e712fbd0f2adf39bf1cdd2c58803978adaf85881
Wei Wei Zhu <weiw.zhu@estudiante.uam.es>
2025-10-12 20:17:15 +0200

    Add files via upload

---
+++ ethmsg.py
@@ -0,0 +1,95 @@
+'''
+	ethmsg.py
+	Implementación del protocolo de mensajeria basica para emision de mensajes en tiempo real sobre ethernet.
+	Autor: Manuel Ruiz <manuel.ruiz.fernandez@uam.es>
+	2024 EPS-UAM
+'''
+
+from ethernet import *
+import logging
+import socket
+import struct
+import fcntl
+import time
+from threading import Lock
+from expiringdict import ExpiringDict
+
+ETHTYPE = 0x0AA0
+#Dirección de difusión (Broadcast)
+broadcast = bytes([0xFF]*6)
+
+def process_ethMsg_frame(us:ctypes.c_void_p,header:pcap_pkthdr,data:bytes,srcMac:bytes) -> None:
+	'''
+		Nombre: process_EthMsg_frame
+		Descripción: Esta función procesa las tramas mensajes sobre ethernet. 
+			Se ejecutará por cada trama Ethenet que se reciba con Ethertype ETHTYPE (si ha sido registrada en initEth). 
+				- Imprimir el contenido de los datos indicando la direccion MAC del remitente así como el tiempo de recepcion del mensaje, según el siguiente formato:
+					[<segundos.microsegundos>] <MAC>: <mensaje> 
+				- En caso de que no exista retornar
+			
+		Argumentos:
+			-us: Datos de usuario pasados desde la llamada de pcap_loop. En nuestro caso será None
+			-header: cabecera pcap_pktheader
+			-data: array de bytes con el contenido de la trama ethMsg. Los dos primeros bytes tienen la longitud del mensaje en orden de red. El resto de bytes son el mensaje en sí mismo.
+			-srcMac: MAC origen de la trama Ethernet que se ha recibido
+		Retorno: Ninguno
+	'''
+	#Obtenemos la longitud del mensaje (Como es ethernet basico, los datos tienen que ser al menos 60 bytes)
+	length = int.from_bytes(data[0:2], 'big')
+	
+	#Comprobamos que hay mensaje
+	if (length < 1):
+		return
+		
+	#Extraemos e imprimimos mensaje
+	msg = ''.join(chr(b) for b in data[2:2+length])
+	macStr = "{:02}:{:02}:{:02}:{:02}:{:02}:{:02}".format(srcMac[0],srcMac[1],srcMac[2],srcMac[3],srcMac[4],srcMac[5])
+	print("[" + str(header.ts.tv_sec + (header.ts.tv_usec/1000000)) + "] " + macStr + ": " + msg)
+	print(">>")
+
+def initEthMsg(interface:str) -> int:
+	'''
+		Nombre: initEthMsg
+		Descripción: Esta función construirá inicializará el nivel ethMsg. Esta función debe realizar, al menos, las siguientes tareas:
+			-Registrar la función del callback process_ethMsg_frame con el Ethertype ETHTYPE
+		Argumentos:   
+			interfaz
+	'''
+	registerEthCallback(process_ethMsg_frame, ETHTYPE)
+	return 0
+
+def sendEthMsg(message:bytes) -> bytes:
+	'''
+		Nombre: sendEthMsg
+		Descripción: Esta función mandara un mensaje en broacast 
+			
+			Esta función debe realizar, al menos, las siguientes tareas:
+				- Crear una trama Ehernet con el mensaje remitido. Los datos de la trama tienen que incluir la longitud del mensaje en orden de red, seguido del mensaje.
+				- Enviar un mensaje en broadcast. La llamada a Ethernet debe tener en cuenta la longitud total (longitud+mensaje)
+		Argumentos:
+			message: datos con el mensaje a remitir.
+				
+		Retorno: 
+			Numero de Bytes transmitidos en el mensaje.
+			None en caso de que no haya podido emitir el mensaje
+				
+		  
+	'''
+	data = bytearray()
+	data[0:2] = len(message).to_bytes(2, 'big')
+	data[2:] = bytearray(message, "ascii")
+	
+	rst = sendEthernetFrame(data, len(data), ETHTYPE, broadcast)
+	if (rst < 0):
+		return None
+	
+	return len(data)
+
+def stopEthMsg()->int:
+	'''
+		Nombre: stopEthMsg
+		Descripción: Esta función termina el nivel EthMsg (elimina su callback del ethernet)
+		Retorno: 
+			-1 en caso de error, 0 en otro caso
+	'''
+	return removeEthCallback(ETHTYPE)
